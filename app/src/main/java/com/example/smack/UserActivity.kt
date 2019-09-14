256af85f509b342a83b722f94e850983f36713ae
iDemetrus <workhome.pro@gmail.com>
2019-09-14 22:05:51 +0300

    add logic into UserActivity for avatar icon

--- app/src/main/java/com/example/smack/UserActivity.kt
+++ app/src/main/java/com/example/smack/UserActivity.kt
@@ -1,11 +1,18 @@
 package com.example.smack
 
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import kotlinx.android.synthetic.main.activity_user.*
+import kotlin.random.Random.Default.nextInt
 
 class UserActivity : AppCompatActivity() {
+
+    private var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    private var userAvatar = "profileDefault"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +26,32 @@ class UserActivity : AppCompatActivity() {
         //Add click listener on create background color button
         create_background_color_button.setOnClickListener {
             Unit
-            Toast.makeText(this, "Create background color clicked", Toast.LENGTH_SHORT).show()
+            //Set random colors
+            val r = nextInt(255)
+            val g = nextInt(255)
+            val b = nextInt(255)
+            create_user_avatar_image.setBackgroundColor(Color.rgb(r, g, b))
+
+            //Save colors for add into database later
+            val savedR = r.toDouble() / 255
+            val savedG = g.toDouble() / 255
+            val savedB = b.toDouble() / 255
+            avatarColor = "[$savedR, $savedG,$savedB, 1]"
         }
         //Add click listener on create avatar button
         create_user_avatar_image.setOnClickListener {
             Unit
-            Toast.makeText(this, "Create user avatar clicked", Toast.LENGTH_SHORT).show()
+            //Set an user avatar image
+            val color = nextInt(2)
+            val avatar = nextInt(28)
+            userAvatar = if (color == 0) {
+                "light$avatar"
+            } else {
+                "dark$avatar"
+            }
+            val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+            create_user_avatar_image.setImageResource(resourceId)
+
         }
     }
 }
