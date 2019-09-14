6b14d05aafc631542c7c14aeacb7bf1df5eccbe9
iDemetrus <workhome.pro@gmail.com>
2019-09-14 14:27:08 +0300

    changed navigation bar

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.smack
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,10 +20,28 @@ class MainActivity : AppCompatActivity() {
         //Find a drawer layout
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         //Set toggle action bar to "Open" and "Close" action
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.nav_drawer_open,
+            R.string.nav_drawer_close
+        )
         //Add toggle as a listener for drawer layout
         drawerLayout.addDrawerListener(toggle)
         //Set sync state action bar
         toggle.syncState()
+
+        //Add click listener on the login button
+        login_button_nav.setOnClickListener {
+            Unit
+            Toast.makeText(this, "Login clicked", Toast.LENGTH_SHORT).show()
+        }
+        //Add click listener on the add channel button
+        channel_add_button_nav.setOnClickListener {
+            Unit
+            Toast.makeText(this, "Add channel clicked", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
