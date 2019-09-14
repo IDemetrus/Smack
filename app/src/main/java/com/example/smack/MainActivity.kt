5159447550b81e6dac26bf9e88d05451ecac9cb0
IDemetrus <workhome.pro@gmail.com>
2019-09-14 12:43:36 +0300

    Add toogle bar

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,34 +1,32 @@
 package com.example.smack
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import android.view.Menu
-import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration1
-import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration2
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    //init a drawer layout
+    lateinit var drawerLayout: DrawerLayout
+    //init a toggle action bar
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-    }
+        toolbar.setNavigationIcon(R.drawable.ic_nav_menu)
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_view)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        //Find a drawer layout
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        //Set toggle action bar to "Open" and "Close" action
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close)
+        //Add toggle as a listener for drawer layout
+        drawerLayout.addDrawerListener(toggle)
+        //Set sync state action bar
+        toggle.syncState()
     }
 }
