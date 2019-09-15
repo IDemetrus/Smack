9fa944650899ba29bb4afa2dfe517e7d5dd67254
iDemetrus <workhome.pro@gmail.com>
2019-09-15 14:51:18 +0300

    Add a login request

--- app/src/main/java/com/example/smack/controller/LoginActivity.kt
+++ app/src/main/java/com/example/smack/controller/LoginActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smack.R
+import com.example.smack.services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_user.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,8 +17,18 @@ class LoginActivity : AppCompatActivity() {
 
         //Add click listener on login button
         login_button.setOnClickListener {
+            val userEmail = login_email_text.text.toString()
+            val userPassword = login_password_text.text.toString()
             Unit
-            Toast.makeText(this, "Login clicked", Toast.LENGTH_SHORT).show()
+            AuthService.loginUser(this, userEmail, userPassword) { complete ->
+                if (complete) {
+                    Toast.makeText(this, "Successfully log in as: $userEmail", Toast.LENGTH_SHORT).show()
+                    val intentMain = Intent(this, MainActivity::class.java)
+                    startActivity(intentMain)
+                }else{
+                    Toast.makeText(this, "Could not log in as: $userEmail", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
         //Add click listener on create button
         sign_up_button.setOnClickListener {
--- app/src/main/java/com/example/smack/utils/Constants.kt
+++ app/src/main/java/com/example/smack/utils/Constants.kt
@@ -2,3 +2,4 @@ package com.example.smack.utils
 
 const val BASE_URL = "http://192.168.43.168:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
