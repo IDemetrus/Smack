d8d40d8de267f068198289bbe090e77b94bd6471
IDemetrus <workhome.pro@gmail.com>
2019-09-22 15:19:28 +0300

    changed url cfg

--- app/src/main/java/com/example/smack/utils/Constants.kt
+++ app/src/main/java/com/example/smack/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.smack.utils
 
-const val BASE_URL = "http://192.168.43.168:3005/v1/"
+// For heroku access
+const val BASE_URL = "https://api-for-chat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
