65c6f8bac856649e8589af65aed39f90ccb41af6
Bonacci <workhome.pro@gmail.com>
2019-09-25 16:13:26 +0300

    add Pinky colors

--- app/src/main/java/com/example/smack/utils/Constants.kt
+++ app/src/main/java/com/example/smack/utils/Constants.kt
@@ -2,8 +2,11 @@ package com.example.smack.utils
 
 // For heroku access
 //const val BASE_URL = "https://api-for-chat.herokuapp.com/v1/"
-// For local access
-const val BASE_URL = "http://192.168.42.211:3005/v1/"
+
+// For local access Linux PC
+// const val BASE_URL = "http://192.168.42.211:3005/v1/"
+// For local access Windows
+const val BASE_URL = "http://192.168.42.67:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
